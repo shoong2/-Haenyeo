215192ecbed155455bcda7d5aca90e481633af98
Eon_2e <43786927+shoong2@users.noreply.github.com>
2023-04-07 12:29:23 +0900

    [0407]

---
+++ Haenyeo/Assets/Scripts/UIScripts/Slot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Slot : MonoBehaviour
+{
+    //public Item item; //획득한 아이템
+    public Image itemImage; // 아이템의 이미지
+
+    public void AddItem()
+    {
+
+    }
+}
