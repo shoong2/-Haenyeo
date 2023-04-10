58186ddd41597bb811dc0f05d40c35a5f1b78f0a
Eon_2e <43786927+shoong2@users.noreply.github.com>
2023-04-11 04:16:47 +0900

    [0411]

--- Haenyeo/Assets/Scripts/Player.cs
+++ Haenyeo/Assets/Scripts/Player.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public VariableJoystick joy;
-    public float speed = 1f;
-    Rigidbody2D rigid;
-    Vector2 moveVec;
+    public VariableJoystick joy; //조이스틱
+
+    public float speed = 1f; //움직임 속도
+    float objectWidth;
+    float objectHeight;
+
+    Rigidbody2D rigid; 
+
+    Vector2 moveVec; 
+
+    SpriteRenderer render; //좌우반전 스프라이트 렌더러
+
+    Camera mainCamera;
+
     private void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        render = GetComponent<SpriteRenderer>();
+        mainCamera = Camera.main;
+        objectWidth = transform.localScale.x;
+        objectHeight = transform.localScale.y;
     }
 
     private void FixedUpdate()
@@ -18,7 +32,25 @@ public class Player : MonoBehaviour
         float x = joy.Horizontal;
         float y = joy.Vertical;
 
-        moveVec = new Vector2(x, y) * speed * Time.deltaTime;
-        rigid.MovePosition(rigid.position + moveVec);
+        //좌우반전
+        if (x >= 0)
+            render.flipX = true;
+        else
+            render.flipX = false;
+
+        moveVec = rigid.position + new Vector2(x, y) * speed * Time.deltaTime;
+       
+        //카메라 밖으로 나가지 않도록 제한
+        float leftEdge = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + objectWidth / 2;
+        float rightEdge = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - objectWidth / 2;
+        float topEdge = mainCamera.ViewportToWorldPoint(new Vector3(0, 0.68f, 0)).y - objectHeight / 2;
+        float bottomEdge = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + objectHeight / 2;
+
+        moveVec = new(Mathf.Clamp(moveVec.x, leftEdge, rightEdge), 
+            Mathf.Clamp(moveVec.y, bottomEdge, topEdge));
+
+        rigid.MovePosition(moveVec);
     }
+
+
 }
