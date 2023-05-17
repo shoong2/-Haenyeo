63062b870b5f6bc821770f95af3a96806e920a4d
Eon_2e <43786927+shoong2@users.noreply.github.com>
2023-05-17 19:42:45 +0900

    [0517]

--- Haenyeo/Assets/Scripts/Dialogue/DialogueParser.cs
+++ Haenyeo/Assets/Scripts/Dialogue/DialogueParser.cs
@@ -11,21 +11,53 @@ public class DialogueParser : MonoBehaviour
 
         string[] data = csvData.text.Split(new char[] { '\n' }); //엔터 단위로 쪼개서 가져옴
         
-        for(int i=1; i<data.Length;)
+        //for(int i=1; i<data.Length;)
+        //{
+        //    string[] row = data[i].Split(new char[] { ',' });
+
+        //    Dialogue dialogue = new Dialogue(); //대사 리스트 생성
+
+        //    dialogue.name = row[1];
+        //    Debug.Log(row[1]);
+
+        //    List<string> contextList = new List<string>();
+
+        //    do
+        //    {
+        //        contextList.Add(row[2]);
+        //        Debug.Log(row[2]);
+        //        if (++i < data.Length)
+        //        {
+        //            row = data[i].Split(new char[] { ',' });
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    while (row[0].ToString() == "");
+
+        //    dialogue.contexts = contextList.ToArray();
+
+        //    dialogueList.Add(dialogue);
+         
+        //}
+
+        for(int i=2; i<data.Length;)
         {
             string[] row = data[i].Split(new char[] { ',' });
 
+
             Dialogue dialogue = new Dialogue(); //대사 리스트 생성
 
-            dialogue.name = row[1];
-            Debug.Log(row[1]);
-
+            dialogue.name = row[5];
+            Debug.Log(row[5]);
             List<string> contextList = new List<string>();
 
             do
             {
-                contextList.Add(row[2]);
-                Debug.Log(row[2]);
+                contextList.Add(row[6]);
+                Debug.Log(row[6]);
                 if (++i < data.Length)
                 {
                     row = data[i].Split(new char[] { ',' });
@@ -35,16 +67,15 @@ public class DialogueParser : MonoBehaviour
                     break;
                 }
             }
-            while (row[0].ToString() == "");
+            while (row[5].ToString() == "");
 
             dialogue.contexts = contextList.ToArray();
 
             dialogueList.Add(dialogue);
-         
+
         }
 
         return dialogueList.ToArray();
     }
 
-  
 }
